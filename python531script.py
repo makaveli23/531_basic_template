a51514203848832b60d85599ba2d0ea3a81637a1
makaveli <jopferreira1997@gmail.com>
2022-10-30 17:34:13 +0000

    added menu, and organized the code

--- python531script.py
+++ python531script.py
@@ -6,46 +6,119 @@ def roundToPlates(n):
     basenmr = n%10
 
 
-def readTMs(array, file):
+def readTMs(valueArray, file):
     f = open(file, "r")
-    array.append(float(f.read())) 
+    valueArray.append(float(f.read())) 
     f.close()
 
 
-def printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults):
+def printSet(multiplierArray, tmValue, repsArray):
+    print("(" + str(int(multiplierArray[0]*100)) +  "%) = " + str(round(tmValue*multiplierArray[0], 2)) + "kg x" + str(repsArray[0]))
+    print("(" + str(int(multiplierArray[1]*100)) +  "%) = " + str(round(tmValue*multiplierArray[1], 2)) + "kg x" + str(repsArray[1]))
+    print("(" + str(int(multiplierArray[2]*100)) +  "%) = " + str(round(tmValue*multiplierArray[2], 2)) + "kg x" + str(repsArray[2]))
+    
+
+def printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults, repsWarmup, weekNmr):
+    print("------------------------------------")
+    print("Week " + weekNmr +":")
+    print()
+
     counter = 0
     for ex in arrayEx:
         print("----- " + arrayExName[counter] + " Training Max = " + str(ex) + " -----")
         if (counter % 2 == 0):
-        #if (1): #for warmup weeks, comment this and uncomment above line for normal weeks
-            print("warmup 1(" + str(int(warmupMults[0]*100)) +  "%) = " + str(round(ex*warmupMults[0], 2)) + "kg x5")
-            print("warmup 2(" + str(int(warmupMults[1]*100)) +  "%) = " + str(round(ex*warmupMults[1], 2)) + "kg x5")
-            print("warmup 3(" + str(int(warmupMults[2]*100)) +  "%) = " + str(round(ex*warmupMults[2], 2)) + "kg x3")
-            print("first set(" + str(int(setMultML[0]*100)) +  "%) = " + str(round(ex*setMultML[0], 2)) + "kg x" + str(repsML[0]))
-            print("second set(" + str(int(setMultML[1]*100)) +  "%) = " + str(round(ex*setMultML[1], 2)) + "kg x" + str(repsML[1]))
-            print("last set(" + str(int(setMultML[2]*100)) +  "%) = " + str(round(ex*setMultML[2], 2)) + "kg x" + str(repsML[2]))
+            printSet(warmupMults, ex, repsWarmup) 
+            printSet(setMultML, ex, repsML)
         else:
-            print("first set(" + str(int(setMultAL[0]*100)) +  "%) = " + str(round(ex*setMultAL[0], 2)) + "kg x" + str(repsAL[0]))
-            print("second set(" + str(int(setMultAL[1]*100)) +  "%) = " + str(round(ex*setMultAL[1], 2)) + "kg x" + str(repsAL[1]))
-            print("last set(" + str(int(setMultAL[2]*100)) +  "%) = " + str(round(ex*setMultAL[2], 2)) + "kg x" + str(repsAL[2]))
-     
+            printSet(setMultAL, ex, repsAL)
+        
         print("----------------------------------------")
         print()
         counter+=1
      
+    print()
+    print("------------------------------------")
+
+
+def printCycleWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults, repsWarmup):
+    # ------------------ calculate and show values for week1 ------------------------- 
+    setMultML = [0.65, 0.75, 0.85]
+    repsML = [5, 5, 5]
+
+    setMultAL = [0.5, 0.6, 0.7]
+    repsAL = [10, 10, 10]
+
+    printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults, repsWarmup, "1")
+    # --------------------------------------------------------------------------------
+
+    # ------------------ calculate and show values for week2 ------------------------- 
+    setMultML = [0.7, 0.8, 0.9]
+    repsML = [3, 3, 3]
+
+    setMultAL = [0.6, 0.7, 0.8]
+    repsAL = [8, 8, 6]
+
+    printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults, repsWarmup, "2")
+    # --------------------------------------------------------------------------------
+
+    # ------------------ calculate and show values for week3 ------------------------- 
+    setMultML = [0.75, 0.85, 0.95]
+    repsML = [5, 3, 1]
+
+    setMultAL = [0.65, 0.75, 0.85]
+    repsAL = [5, 5, 5]
+
+    printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults, repsWarmup, "3")
+    # --------------------------------------------------------------------------------
+
+
+def viewMaxes(arrayValue, arrayName):
+    counter = 0
+    for value in arrayValue:
+        print()
+        print("Your " + arrayName[counter] + " Training Max is: " + str(round(value, 2)))
+        print("Your " + arrayName[counter] + " One Rep Max is: " + str(round(value/0.9, 2)))
+        counter+=1
+
+
+def menu():
+    print()
+    print("Choose your option:")
+    option = input("\t1 - Print cycle work\n\t2 - View TM and 1RPMs\n\t3 - Increase TMs(TODO)\n\t0- Exit\nValue(1, 2, 3 or 0): ")
+    
+    if (option == "0"):
+        print("Goodbye") 
+        return 1
+
+    if (option == "1"):
+        printCycleWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults, repsWarmup)
+        return menu()
+    
+    if (option == "2"):
+        viewMaxes(arrayEx, arrayExName)
+        return menu()
+        
+    if (option == "3"):
+        print("todo")
+        return menu()
+
+
+    print("You inserted a wrong value, only available options are 1, 2, 3 or 0, try again")
+    return menu()
 
 
 
-#constant values
-
+# --------- initialize constant values -------------------------
 arrayEx = []
 arrayExName = ["OHP", "CGBench", "Deadlift", "Front Squat", "Bench", "InclineB", "Squat", "RDL"]
 
-
 warmupMults = [0.4, 0.5, 0.6]
+repsWarmup = [5, 5, 3]
 
-
-
+setMultML = []
+repsML = []
+setMultAL = []
+repsAL = []
 
 #--------------- grab training maxes from files -----------------------------
 readTMs(arrayEx, "trainingMaxes/ohpTM.txt")
@@ -56,72 +129,10 @@ readTMs(arrayEx, "trainingMaxes/benchTM.txt")
 readTMs(arrayEx, "trainingMaxes/ibenchTM.txt")
 readTMs(arrayEx, "trainingMaxes/squatTM.txt")
 readTMs(arrayEx, "trainingMaxes/rdlTM.txt")
+
 # -----------------------------------------------------------------------
 
 
-
-#calculate and show values for week1
-
-setMultML = [0.65, 0.75, 0.85]
-repsML = [5, 5, 5]
-
-setMultAL = [0.5, 0.6, 0.7]
-repsAL = [10, 10, 10]
-
-print("------------------------------------")
-print("Week 1:")
-print()
-printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults)
-print()
-print("------------------------------------")
-
-
-#calculate and show values for week2
-
-setMultML = [0.7, 0.8, 0.9]
-repsML = [3, 3, 3]
-
-setMultAL = [0.6, 0.7, 0.8]
-repsAL = [8, 8, 6]
-
-print("------------------------------------")
-print("Week 2:")
-print()
-printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults)
-print()
-print("------------------------------------")
-
-
-#calculate and show values for week3
-
-setMultML = [0.75, 0.85, 0.95]
-repsML = [5, 3, 1]
-
-setMultAL = [0.65, 0.75, 0.85]
-repsAL = [5, 5, 5]
-
-print("------------------------------------")
-print("Week 3:")
-print()
-printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL, warmupMults)
-print()
-print("------------------------------------")
-
-
-
-#calculate and show values for week4
-
-#setMultML = [0.65, 0.75, 0.85]
-#repsML = [5, 5, 5]
-#
-#setMultAL = [0.6, 0.7, 0.8]
-#repsAL = [8, 8, 6]
-#
-#print("------------------------------------")
-#print("Week 4:")
-#print()
-#printWeeklyWork(arrayEx, arrayExName, setMultML, setMultAL, repsML, repsAL)
-#print()
-#print("------------------------------------")
+menu()
 
 
